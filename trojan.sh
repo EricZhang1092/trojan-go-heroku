732a0e5651e618a4e651a11661c759836396245f
EricZhang1092 <66500708+EricZhang1092@users.noreply.github.com>
2022-01-22 12:28:40 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -2,28 +2,6 @@
 
 # config trojan
 install -d /etc/trojan-go/
-cat << EOF > /etc/trojan-go/config.json
-{
-    "run_type": "server",
-    "local_addr": "0.0.0.0",
-    "local_port": $PORT,
-    "remote_addr": "www.apple.com",
-    "remote_port": 80,
-    "log_level": 0,
-    "password": [
-	  "$PASSWORD"
-    ],
-    "ssl":{
-      "cert"
-      "key"
-  },
-  $CUSTOM
-    "websocket": {
-    "enabled": true,
-    "path": "/shop"
-    }
-}
-EOF
 
 # run trojan
-/usr/local/bin/trojan-go -config /etc/trojan-go/config.json
+/usr/local/bin/trojan-go -config /tro/config.json
